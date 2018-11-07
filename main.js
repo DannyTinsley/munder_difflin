0f479b9cecfb88b502b13e6c41748ade231bdd07
Danielle Tinsley <dtinsley000@nyc.yearup.org>
2018-11-07 12:33:59 -0500

    done

--- main.js
+++ main.js
@@ -14,16 +14,44 @@
 //     setTimeout(slideshow, 2000);
 // }
 
-var myIndex = 0;
+
+
+// Scroll lo lo lo
+window.onscroll = function() {scroll()};
+
+function scroll() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+      document.getElementsByClassName("pleaseScroll").className = "pleaseScroll";
+}
+
+// End scroll function
+
+
+
+// moving gallery
+let myIndex = 0;
+
 carousel();
 
 function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
+    let i;
+    let x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}
     x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 2000); 
+    setTimeout(carousel, 2000);
+
+    carousel ();
+
+// now, for the button
+document.getElementById("clickhere").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("clickhere").innerHTML = "YOU CLICKED ME!";
+
+}
+
+    }
